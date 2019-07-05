877fcae0bc947433d3afbc00afba497da1e00403
Michal Snopek <snuu12@gmail.com>
2019-07-05 18:37:24 +0200

    modify gitignore

---
+++ task1.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+
+        Random random=new Random();
+
+        System.out.println(random.nextInt(100)+1);
+
+    }
+
+
+
+}
+
+
